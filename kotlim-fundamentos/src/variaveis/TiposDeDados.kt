a13491e21f03c00b732e8933aa13700cdd30a8d4
Thomas Neves <thomas_neves@ymail.com>
2025-01-07 16:28:58 -0300

    Aula um e dois
    estudo de Kotlim:
    aula um fundamentos de variáveis e como usa-las,
    aula dois fundamentos de tipos de dados.

---
+++ kotlim-fundamentos/src/variaveis/TiposDeDados.kt
@@ -0,0 +1,48 @@
+package variaveis
+
+fun main() {
+    // Tipos númericos
+    /*
+    * Byte      - 8
+    * Short     - 16
+    * Int       - 32
+    * Long      - 64
+    * Float     - 32
+    * Double    - 64
+     */
+
+    val byte: Byte = 8
+    val short: Short = 360
+    val int: Int = 1000
+    val long: Long = 10000
+    val float: Float = 150.10F
+    val double: Double = 300.10
+
+    println(byte)
+    println(short)
+    println(int)
+    println(long)
+    println(float)
+    println(double)
+
+    // Tipo Booleano
+    /*
+    * True False
+     */
+
+    val boolean: Boolean = false
+
+    println(boolean)
+
+    // Tipos textuais
+    /*
+    * Char quarda um unico caracter e usa aspas simples
+    * String quarda uma cadeia de caracteres e usa aspas duplas
+     */
+
+    val char: Char = 'c'
+    val string: String = "Thomas"
+
+    println(char)
+    println(string)
+}
---
+++ kotlim-fundamentos/src/variaveis/Variaveis.kt
@@ -0,0 +1,10 @@
+package variaveis
+
+fun main() {
+    var name = "Thomas"
+    val lasName = "Pereira" /*Variaveis do tipo Var podem ter servalor auterado, já variaveis do tipo Val não podem sendo assim uma constante*/
+
+    println(name)
+    println(lasName)
+}
+
